ee9ab34481e85c891caf5fe549f8ed9eadcf4ddd
Alberto Anceschi <anceschialberto@me.com>
2022-01-16 22:40:15 +0100

    05 hitcounter construct definition

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -1,5 +1,7 @@
 import { Construct } from 'constructs';
 import { aws_lambda as lambda } from 'aws-cdk-lib';
+import { aws_lambda_nodejs } from 'aws-cdk-lib';
+import { aws_dynamodb as dynamodb } from 'aws-cdk-lib';
 
 export interface HitCounterProps {
   /** the function for which we want to count url hits **/
@@ -7,9 +9,24 @@ export interface HitCounterProps {
 }
 
 export class HitCounter extends Construct {
+  public readonly handler: lambda.Function;
+
+
   constructor(scope: Construct, id: string, props: HitCounterProps) {
     super(scope, id);
 
-    // TODO
+    const table = new dynamodb.Table(this, 'Hits', {
+      partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
+    });
+
+    this.handler = new aws_lambda_nodejs.NodejsFunction(this, 'HitCounterHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      entry: 'lambda/hitcounter.ts',
+      handler: 'handler',
+      environment: {
+        DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+        HITS_TABLE_NAME: table.tableName
+      }
+    });
   }
 }
