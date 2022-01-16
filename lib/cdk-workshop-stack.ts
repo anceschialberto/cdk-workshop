6fbe0e85cf5ee4b524bc6b8ce4dea9bb26152c21
Alberto Anceschi <anceschialberto@me.com>
2022-01-16 21:36:20 +0100

    02 add lambda to stack

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,9 +1,15 @@
-import * as cdk from 'aws-cdk-lib';
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { aws_lambda } from 'aws-cdk-lib';
+import { aws_lambda_nodejs } from 'aws-cdk-lib';
 
-export class CdkWorkshopStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+export class CdkWorkshopStack extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // nothing here!
+    const hello = new aws_lambda_nodejs.NodejsFunction(this, 'HelloHandler', {
+      runtime: aws_lambda.Runtime.NODEJS_14_X,
+      entry: "lambda/hello.ts",
+      handler: "handler",
+    })
   }
 }
