d594b3f11f9dc1df53375da6e151255bfbf50d29
Alberto Anceschi <anceschialberto@me.com>
2022-01-21 14:05:31 +0100

    feat: cicd

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
+import { PipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
-new CdkWorkshopStack(app, 'CdkWorkshopStack');
+new PipelineStack(app, 'CdkWorkshopStack-CICD');
--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,4 +1,5 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { aws_lambda } from 'aws-cdk-lib';
 import { aws_lambda_nodejs } from 'aws-cdk-lib';
 import { aws_apigateway as apigw } from 'aws-cdk-lib';
@@ -7,7 +8,7 @@ import { HitCounter } from './hitcounter';
 import { Monitoring } from './monitoring';
 
 export class CdkWorkshopStack extends Stack {
-  constructor(scope: App, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     const hello = new aws_lambda_nodejs.NodejsFunction(this, 'HelloHandler', {
